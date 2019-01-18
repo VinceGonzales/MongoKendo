aef1b6b5314eb0f7316d104d79ce2ee33b452d20
VinceGonzales <vince.gonzales@hotmail.com>
2019-01-18 09:58:23 -0800

    Captcha

---
+++ Rad.Web/App_Start/WebApiConfig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net.Http.Headers;
+using System.Web.Http;
+
+namespace Rad.Web
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.MapHttpAttributeRoutes();
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("multipart/form-data"));
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Rad.Web/Controllers/CaptchaController.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Drawing.Text;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web.Http;
+
+namespace Rad.Web.Controllers
+{
+    public class CaptchaController : ApiController
+    {
+        [HttpGet]
+        public HttpResponseMessage Get()
+        {
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK);
+
+            int iHeight = 70;
+            int iWidth = 190;
+            Random oRandom = new Random();
+
+            int[] aBackgroundNoiseColor = new int[] { 150, 150, 150 };
+            int[] aTextColor = new int[] { 0, 0, 0 };
+            int[] aFontEmSizes = new int[] { 15, 20, 25, 30, 35 };
+
+            string[] aFontNames = new string[] { "Comic Sans MS", "Arial", "Times New Roman", "Georgia", "Verdana", "Geneva" };
+            FontStyle[] aFontStyles = new FontStyle[] { FontStyle.Bold, FontStyle.Italic, FontStyle.Regular, FontStyle.Strikeout, FontStyle.Underline };
+            HatchStyle[] aHatchStyles = new HatchStyle[] { HatchStyle.BackwardDiagonal, HatchStyle.Cross, HatchStyle.DashedDownwardDiagonal, HatchStyle.DashedHorizontal, HatchStyle.DashedUpwardDiagonal, HatchStyle.DashedVertical, HatchStyle.DiagonalBrick, HatchStyle.DiagonalCross, HatchStyle.Divot, HatchStyle.DottedDiamond, HatchStyle.DottedGrid, HatchStyle.ForwardDiagonal, HatchStyle.Horizontal, HatchStyle.HorizontalBrick, HatchStyle.LargeCheckerBoard, HatchStyle.LargeConfetti, HatchStyle.LargeGrid, HatchStyle.LightDownwardDiagonal, HatchStyle.LightHorizontal, HatchStyle.LightUpwardDiagonal, HatchStyle.LightVertical, HatchStyle.Max, HatchStyle.Min, HatchStyle.NarrowHorizontal, HatchStyle.NarrowVertical, HatchStyle.OutlinedDiamond, HatchStyle.Plaid, HatchStyle.Shingle, HatchStyle.SmallCheckerBoard, HatchStyle.SmallConfetti, HatchStyle.SmallGrid, HatchStyle.SolidDiamond, HatchStyle.Sphere, HatchStyle.Trellis, HatchStyle.Vertical, HatchStyle.Wave, HatchStyle.Weave, HatchStyle.WideDownwardDiagonal, HatchStyle.WideUpwardDiagonal, HatchStyle.ZigZag };
+
+            //Get Captcha
+            string sCaptchaText = SetCaptchaText();
+
+            //Creates an output Bitmap
+            Bitmap oOutputBitmap = new Bitmap(iWidth, iHeight, PixelFormat.Format24bppRgb);
+            Graphics oGraphics = Graphics.FromImage(oOutputBitmap);
+            oGraphics.TextRenderingHint = TextRenderingHint.AntiAlias;
+
+            //Create a Drawing area
+            RectangleF oRectangleF = new RectangleF(0, 0, iWidth, iHeight);
+            Brush oBrush = default(Brush);
+
+            //Draw background (Lighter colors RGB 100 to 255)
+            oBrush = new HatchBrush(aHatchStyles[oRandom.Next
+                (aHatchStyles.Length - 1)], Color.FromArgb((oRandom.Next(100, 255)),
+                (oRandom.Next(100, 255)), (oRandom.Next(100, 255))), Color.White);
+            oGraphics.FillRectangle(oBrush, oRectangleF);
+
+            Matrix oMatrix = new Matrix();
+            int i = 0;
+            for (i = 0; i <= sCaptchaText.Length - 1; i++)
+            {
+                oMatrix.Reset();
+                int iChars = sCaptchaText.Length;
+                int x = iWidth / (iChars + 1) * i;
+                int y = iHeight / 2;
+
+                //Rotate text Random
+                oMatrix.RotateAt(oRandom.Next(-40, 40), new PointF(x, y));
+                oGraphics.Transform = oMatrix;
+
+                //Draw the letters with Random Font Type, Size and Color
+                oGraphics.DrawString
+                (
+                //Text
+                sCaptchaText.Substring(i, 1),
+                //Random Font Name and Style
+                new Font(aFontNames[oRandom.Next(aFontNames.Length - 1)],
+                   aFontEmSizes[oRandom.Next(aFontEmSizes.Length - 1)],
+                   aFontStyles[oRandom.Next(aFontStyles.Length - 1)]),
+                //Random Color (Darker colors RGB 0 to 100)
+                new SolidBrush(Color.FromArgb(oRandom.Next(0, 100),
+                   oRandom.Next(0, 100), oRandom.Next(0, 100))),
+                x,
+                oRandom.Next(10, 40)
+                );
+                oGraphics.ResetTransform();
+            }
+
+            MemoryStream oMemoryStream = new MemoryStream();
+            oOutputBitmap.Save(oMemoryStream, ImageFormat.Png);
+            //byte[] oBytes = oMemoryStream.GetBuffer();
+            oMemoryStream.Position = 0;
+
+            response.Content = new StreamContent(oMemoryStream);
+            response.Content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("image/png");
+            response.Content.Headers.ContentLength = oMemoryStream.Length;
+            response.Headers.AddCookies(new CookieHeaderValue[] { SetCaptchaCookie(sCaptchaText) });
+
+            return response;
+        }
+
+        private string SetCaptchaText()
+        {
+            Random oRandom = new Random();
+            int iNumber = oRandom.Next(100000, 999999);
+            return iNumber.ToString();
+        }
+
+        private CookieHeaderValue SetCaptchaCookie(string captcha)
+        {
+            CookieHeaderValue cookie = new CookieHeaderValue("captcha-text", captcha);
+            cookie.Expires = DateTimeOffset.Now.AddDays(1);
+            cookie.Domain = Request.RequestUri.Host;
+            cookie.Path = "/";
+            return cookie;
+        }
+    }
+}
